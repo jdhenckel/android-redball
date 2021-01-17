b5cd9d8693e209836d2e87349e5155cb96b393b9
John Henckel <Henckel.Jonathan@mayo.edu>
2021-01-17 14:54:14 -0600

    gravity sensor

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,15 +17,37 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyView view;
+    MyGravitySensorListener gravity;
+    SensorManager sensorManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(new MyView2(this));
+        view = new MyView(this);
+        setContentView(view);
+        gravity = new MyGravitySensorListener();
     }
 
 
-    public class MyView2 extends View
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager == null) sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        if (sensor != null)
+            sensorManager.registerListener(gravity, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(gravity);
+    }
+
+    public class MyView extends View
     {
         Paint background;
         Paint ball;
@@ -30,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         float r;
         Timer timer;
 
-        public MyView2(Context context)
+        public MyView(Context context)
         {
             super(context);
         }
@@ -49,11 +75,22 @@ public class MainActivity extends AppCompatActivity {
             ball = new Paint();
             ball.setStyle(Paint.Style.FILL);
             ball.setColor(0xFFCD5C5C);
-            timer = new Timer("animation");
+
+            timer = new Timer("physics update");
             timer.schedule(new TimerTask() {
                 public void run() {
-                    x += vx; if (x > w || x < 0) vx = Math.abs(vx) * Math.signum(-x);
-                    y += vy; if (y > h || y < 0) vy = Math.abs(vy) * Math.signum(-y);
+                    float avx = Math.abs(vx);
+                    float avy = Math.abs(vy);
+                    float minv = 0.01f;
+                    x += vx;
+                    y += vy;
+                    float dt = 0.1f;
+                    if (x + r > w || x < r) vx = avx * Math.signum(r - x);
+                    else vx += gravity.gx * dt;
+                    if (y + r > h || y < r) vy = avy * Math.signum(r - y);
+                    else vy += gravity.gy * dt;
+                    float drag = 0.99f;
+                    vx *= drag; vy *= drag;
                     invalidate();
                 }
             }, 0, 15);
@@ -67,6 +104,21 @@ public class MainActivity extends AppCompatActivity {
             if (w == 0) firstTime();
             canvas.drawPaint(background);
             canvas.drawCircle(x, y, r, ball);
+        }
+    }
+
+    public class MyGravitySensorListener implements SensorEventListener {
+        float gx,gy,gz;
+        @Override
+        public final void onSensorChanged(SensorEvent event) {
+            gx = -event.values[0];
+            gy = event.values[1];
+            gz = event.values[2];
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
         }
     }
 }
